77f85923eb2ced599172ce97823c5041780e5fbe
raivox <ouchkaihsmail@gmail.com>
2024-02-10 23:17:03 +0100

    made some changes in projects section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,42 +1,10 @@
 import React from 'react'
 import ListStyle from '../education/ListStyle'
 import ProjectList from './ProjectList'
+import projects from './../../data/projects.json';
 
 function Projects() {
-    let projectsList = [
-        {
-            title : 'TypeCliGenius',
-            path: 'https://github.com/ouchkaih/TypeCliGenius',
-            description: 'TypeCliGenius is a lightweight command-line interface (CLI) tool developed in TypeScript. This tool provides a straightforward way to perform various calculations and operations directly from the command line.',
-            skills: [
-                'TypeScript', 'Commanderjs', 'Figlet'
-            ],
-        },
-        {
-            title : 'TypeCliGenius',
-            path: 'https://github.com/ouchkaih/TypeCliGenius',
-            description: 'TypeCliGenius is a lightweight command-line interface (CLI) tool developed in TypeScript. This tool provides a straightforward way to perform various calculations and operations directly from the command line.',
-            skills: [
-                'TypeScript', 'Commanderjs', 'Figlet'
-            ],
-        },
-        {
-            title : 'TypeCliGenius',
-            path: 'https://github.com/ouchkaih/TypeCliGenius',
-            description: 'TypeCliGenius is a lightweight command-line interface (CLI) tool developed in TypeScript. This tool provides a straightforward way to perform various calculations and operations directly from the command line.',
-            skills: [
-                'TypeScript', 'Commanderjs', 'Figlet'
-            ],
-        }
-        ,{
-            title : 'TypeCliGenius',
-            path: 'https://github.com/ouchkaih/TypeCliGenius',
-            description: 'TypeCliGenius is a lightweight command-line interface (CLI) tool developed in TypeScript. This tool provides a straightforward way to perform various calculations and operations directly from the command line.',
-            skills: [
-                'TypeScript', 'Commanderjs', 'Figlet'
-            ],
-        }
-    ]
+    console.log(projects);
   return (
     <div>
         <div className="grid grid-cols-2 px-1 py-8">
@@ -44,7 +12,7 @@ function Projects() {
                 <h3 className="capitalize text-2xl mb-3 dark:text-[#CCD6F6] font-semibold"><span className="text-[#0A192F] font-bold dark:text-[#5AE5C6] text-base ">2.</span> Projects</h3>
                 <div className=''>
                 {
-                    projectsList.map((project)=>(
+                    projects.projects.map((project)=>(
                         <div className='grid grid-cols-12'>
                             <ListStyle/>
                             <div className='col-span-11 pb-5'>
